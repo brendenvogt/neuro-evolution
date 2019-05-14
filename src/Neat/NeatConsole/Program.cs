4882e88460bfc9af3b6e8b9b0997311dbce67d34
Brenden Vogt <bvogtdev@gmail.com>
2019-05-14 03:22:32 -0700

    added neatsharp lib and base files

---
+++ src/Neat/NeatConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NeatConsole
+{
+    public class MyInput
+    {
+        public float[] Pixels;
+    }
+    public class MyOutput
+    {
+        public float[] Controls => new[] {L, R, U, D, A, B};
+        private float L => 0;
+
+        private float R => 0;
+
+        private float U => 0;
+
+        private float D => 0;
+
+        private float A => 0;
+
+        private float B => 0;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
